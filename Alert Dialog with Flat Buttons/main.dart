a0c9a982923ccf211cae96c8a39f3c4506682b1c
Chuck Lagos <40728553+Chucklagos@users.noreply.github.com>
2020-09-15 21:16:26 -0600

    Alert Dialogs with buttons action captured by console

---
+++ Alert Dialog with Flat Buttons/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(new MaterialApp(
+    home: MyDialog(),
+  ));
+}
+
+class MyDialog extends StatefulWidget {
+  @override
+  _MyDialogState createState() => _MyDialogState();
+}
+
+enum DialogAction { yes, no }
+
+class _MyDialogState extends State<MyDialog>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  String _inputValue = "";
+
+  void _showAlert(String value) {
+    /* Al declarar algo incialmente con un guion bajo indicamos a que es solo accesible a lo que pertenece a la clase donde esta */
+    AlertDialog dialog = new AlertDialog(
+      content: new Text(value),
+      actions: <Widget>[
+        new FlatButton(
+            onPressed: () {
+              _alertResult(DialogAction.yes);
+            },
+            child: new Text("Si")),
+        new FlatButton(
+            onPressed: () {
+              _alertResult(DialogAction.no);
+            },
+            child: new Text("No"))
+      ],
+    );
+
+    showDialog(context: context, child: dialog);
+  }
+
+  void _onChanged(String value) {
+    setState(() {
+      _inputValue = value;
+    });
+  }
+
+  void _alertResult(DialogAction action) {
+    print("Tu seleccion es $action");
+  }
+
+/* ------------------------------------------------------- */
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+/* --------------------------------------------------------- */
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Ejemplo de Alet Dialog"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new TextField(
+                decoration: InputDecoration(hintText: "Ingrese un Texto"),
+                onChanged: (String value) {
+                  _onChanged(value);
+                },
+              ),
+              new RaisedButton(
+                  child: new Text("Lanzar Alerta"),
+                  onPressed: () {
+                    _showAlert(_inputValue);
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
